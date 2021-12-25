9f5c533a6f9c6a52b54a53e9dad376158aa6924f
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2021-12-25 16:53:40 +0700

    Update User Controller

--- app/controllers/userController/services/checkUserCreate.js
+++ app/controllers/userController/services/checkUserCreate.js
@@ -5,25 +5,28 @@ const service = async (body, trx) => {
     validasi.n_username = await trx('public.t_m_user as tmu')
         .first("tmu.n_username")
         .where({
-            "tmu.b_active": true,
-			"tmu.d_deleted_at": null,
             'tmu.n_username': body.n_username
+        })
+        .whereNot({
+            "tmu.c_status": "X"
         })
 
     validasi.e_email = await trx('public.t_m_user as tmu')
         .first("tmu.e_email")
         .where({
-            "tmu.b_active": true,
-			"tmu.d_deleted_at": null,
             'tmu.e_email': body.e_email
+        })
+        .whereNot({
+            "tmu.c_status": "X"
         })
     
     validasi.e_phone_number = await trx('public.t_m_user as tmu')
         .first("tmu.e_phone_number")
         .where({
-            "tmu.b_active": true,
-			"tmu.d_deleted_at": null,
             'tmu.e_phone_number': body.e_phone_number
+        })
+        .whereNot({
+            "tmu.c_status": "X"
         })
 
     return validasi
--- app/controllers/userController/services/checkUserUpdate.js
+++ app/controllers/userController/services/checkUserUpdate.js
@@ -2,23 +2,35 @@ const service = async (body, before, trx) => {
     
     let validasi = {}
     
+    validasi.n_username = await trx('public.t_m_user as tmu')
+        .first("tmu.n_username")
+        .where({
+            'tmu.n_username': body.n_username
+        })
+        .whereNot({
+            'tmu.n_username': before.n_username,
+            "tmu.c_status": "X"
+        })
+
     validasi.e_email = await trx('public.t_m_user as tmu')
         .first("tmu.e_email")
         .where({
-            "tmu.b_active": true,
-			"tmu.d_deleted_at": null,
             'tmu.e_email': body.e_email
         })
-        .whereNot("tmu.e_email", before.e_email)
+        .whereNot({
+            "tmu.e_email": before.e_email,
+            "tmu.c_status": "X"
+        })
     
     validasi.e_phone_number = await trx('public.t_m_user as tmu')
         .first("tmu.e_phone_number")
         .where({
-            "tmu.b_active": true,
-			"tmu.d_deleted_at": null,
             'tmu.e_phone_number': body.e_phone_number
         })
-        .whereNot("tmu.e_phone_number", before.e_phone_number)
+        .whereNot({
+            "tmu.e_phone_number": before.e_phone_number,
+            "tmu.c_status": "X"
+    })
 
     return validasi
 }
