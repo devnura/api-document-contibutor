84d2e7d7ff7fe870e161a51d333504d161c485b1
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-01-15 08:23:11 +0700

    Update user Controller

--- app/controllers/userController/services/findAll.js
+++ app/controllers/userController/services/findAll.js
@@ -12,13 +12,7 @@ const service = async (trx) => {
 		"tmu.b_active",
 		"tmu.i_created_by",
 		"tmu.n_created_by",
-		trx.raw("TO_CHAR(tmu.d_created_at, 'YYYY-MM-DD HH:mm:SS') AS d_created_at"),
-		"tmu.i_updated_by",
-		"tmu.n_updated_by",
-		trx.raw("TO_CHAR(tmu.d_updated_at, 'YYYY-MM-DD HH:mm:SS') AS d_updated_at"),
-		"tmu.i_deleted_by",
-		"tmu.n_deleted_by",
-		trx.raw("TO_CHAR(tmu.d_deleted_at, 'YYYY-MM-DD HH:mm:SS') AS d_deleted_at"))
+		trx.raw("TO_CHAR(tmu.d_created_at, 'YYYY-MM-DD HH:mm:SS') AS d_created_at"))
         .from('public.t_m_user as tmu')
         .leftJoin('public.t_m_group as tmg', function () {
             this.on('tmg.i_id', '=', 'tmu.i_group')
